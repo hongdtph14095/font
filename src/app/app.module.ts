b1674986022099e128e528f6f2e7225b7ec0da16
hongdtph14095 <77144191+hongdtph14095@users.noreply.github.com>
2022-05-14 08:34:37 +0700

    hihi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,29 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './conponemnts/header/header.component';
+import { AboutComponent } from './about/about.component';
 import { HelloComponent } from './hello/hello.component';
-// import Co from '../conponemnts/hello conponemnt'
-
+import { ProductsComponent } from './conponemnts/products/products.component';
+import { ProductDetailComponent } from './conponemnts/product-detail/product-detail.component';
+import { ProductAddComponent } from './conponemnts/product-add/product-add.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HelloComponent
+    HeaderComponent,
+    AboutComponent,
+    HelloComponent,
+    ProductsComponent,
+    ProductDetailComponent,
+    ProductAddComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
