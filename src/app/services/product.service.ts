19529e50187a0d8d89e1db38703ee578da6e33b1
hongdtph14095 <77144191+hongdtph14095@users.noreply.github.com>
2022-05-30 21:03:04 +0700

    update

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -7,25 +7,27 @@ import { IProduct } from '../models/Product';
   providedIn: 'root'
 })
 export class ProductService {
+  API_URL = `http://localhost:3000/products/`
+
   constructor(
     private http: HttpClient
   ) { }
 
   getProduct(id: any): Observable<IProduct> {
     // return data.find(item => item.id === id);
-    return this.http.get<IProduct>(`http://localhost:3000/products/${id}`);
+    return this.http.get<IProduct>(`${this.API_URL}/${id}`);
   }
   getProductList(): Observable<IProduct[]> {
-    return this.http.get<IProduct[]>(`http://localhost:3000/products`);
+    return this.http.get<IProduct[]>(`${this.API_URL}`);
   }
-  removeProduct() {
-
+  removeProduct(id: number): Observable<IProduct> {
+    return this.http.delete<IProduct>(`${this.API_URL}/${id}`);
   }
-  addProduct() {
-
+  addProduct(product: IProduct): Observable<IProduct> {
+    return this.http.post<IProduct>(`${this.API_URL}`, product);
   }
-  updateProduct() {
-
+  updateProduct(product: IProduct): Observable<IProduct> {
+    return this.http.put<IProduct>(`${this.API_URL}/${product.id}`, product);
   }
 }
 
